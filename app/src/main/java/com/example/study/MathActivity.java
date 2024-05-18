0923f7326e299f100d46b9f38aae3c31df1d17ae
Hafiz <hafizursiam@gmail.com>
2024-05-18 23:42:52 +0600

    yml

--- app/src/main/java/com/example/study/MathActivity.java
+++ app/src/main/java/com/example/study/MathActivity.java
@@ -16,7 +16,7 @@ import java.util.Set;
 
 public class MathActivity extends AppCompatActivity {
 
-    Button[] buttons;
+    Button[] buttons,redButtons;
     Set<Integer> clickedButtons;//: A Set to track which buttons have been clicked.
     Integer count = 0;
     MathQuizModel mathQuizModel;
@@ -39,6 +39,52 @@ public class MathActivity extends AppCompatActivity {
                 findViewById(R.id.q10cm)
         };
 
+        redButtons = new Button[]{
+                findViewById(R.id.q1am),
+                findViewById(R.id.q1cm),
+                findViewById(R.id.q1dm),
+
+                findViewById(R.id.q2am),
+                findViewById(R.id.q2cm),
+                findViewById(R.id.q2dm),
+
+                findViewById(R.id.q3am),
+                findViewById(R.id.q3bm),
+                findViewById(R.id.q3dm),
+
+                findViewById(R.id.q4am),
+                findViewById(R.id.q4bm),
+                findViewById(R.id.q4dm),
+
+                findViewById(R.id.q5am),
+                findViewById(R.id.q5bm),
+                findViewById(R.id.q5cm),
+
+                findViewById(R.id.q6am),
+                findViewById(R.id.q6bm),
+                findViewById(R.id.q6dm),
+
+                findViewById(R.id.q7am),
+                findViewById(R.id.q7bm),
+                findViewById(R.id.q7cm),
+
+                findViewById(R.id.q8am),
+                findViewById(R.id.q8bm),
+                findViewById(R.id.q8cm),
+
+                findViewById(R.id.q9am),
+                findViewById(R.id.q9cm),
+                findViewById(R.id.q9dm),
+
+                findViewById(R.id.q10am),
+                findViewById(R.id.q10bm),
+                findViewById(R.id.q10dm)
+
+
+
+
+        };
+
         clickedButtons = new HashSet<>();//HashSet to track button clicks, ensuring each button is only counted once.
 
         // Use the singleton instance of MathQuizModel
@@ -59,6 +105,22 @@ public class MathActivity extends AppCompatActivity {
             });
         }
 
+        //redButtons
+        clickedButtons.clear();
+
+        for (Button button : redButtons) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int buttonId = v.getId();
+                    if (!clickedButtons.contains(buttonId)) {
+                        clickedButtons.add(buttonId);
+                        // Change button background color to green
+                        button.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                    }
+                }
+            });
+        }
         Button submitButton = findViewById(R.id.submath);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
